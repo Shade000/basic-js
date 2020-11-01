89a1d75cf50445ad5fd6a138299eca70f935a113
Shade000 <2543631@mail.ru>
2020-11-01 23:57:28 +0300

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let stack = [];
+    if (Array.isArray(members) !== true) {
+    return false;
+  } else {
+    for (let i = 0; i < members.length; i++) {
+      if (typeof(members[i]) === 'string') {
+        stack.push(members[i].trim()[0].toUpperCase());
+      };
+    };
+    
+    for (let i = 0; i < stack.length - 1; i++) { // Сортировка методом "Пузырька", т.е. сравниваются два соседних элемента и меняются при необходимости
+      temp = '';
+      for (let j = 0; j < stack.length - 1 - i; j++) { // Последний элемент будет самый большой, поэтому на следующей итерации можно делать на i меньше шагов
+        if (stack[j] > stack[j + 1]) {
+          let temp = stack[j + 1];
+          stack[j + 1] = stack[j];
+          stack[j] = temp;
+        };
+      };
+    };
+    return stack.join('');
+  };
 };
